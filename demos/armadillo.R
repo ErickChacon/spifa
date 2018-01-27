36b321ae91b1c28710415c22c64ab5e45b8d22b0
Erick Chacon Montalvan <eral.th07@gmail.com>
2018-01-27 17:57:08 +0000

    likelihhod more effcicient than mvtnorm package

--- demos/armadillo.R
+++ demos/armadillo.R
@@ -31,3 +31,40 @@ dmvnorm(x=c(0,0), mean=c(1,1))
 library(spmirt)
 x <- matrix(0, 7, 5)
 testing(x, 1:10)
+
+
+library(mvtnorm)
+library(microbenchmark)
+sigma <- matrix(c(2,1,1,2), ncol=2)
+z <- c(1.4, 5)
+microbenchmark(
+               mvtnorm::dmvnorm(z, mean=c(1,2), sigma=sigma, log = TRUE),
+               spmirt::dmvnorm(z, mean=c(1,2), sigma=sigma)
+)
+
+colMeans(x)
+var(x)
+
+n <- 1000
+f <- list(
+  prob ~ I(1) +
+    gp(list(s1, s2), cor.model = "exp_cor", cor.params = list(phi = 0.02)),
+  size ~ I(1)
+  )
+data <- sim_model(formula = f,
+                  link_inv = list(pnorm, identity),
+                  generator = rbinom,
+                  n = n,
+                  seed = 1)
+data <- data %>% dplyr::arrange(s1)
+dist <- as.matrix(dist(dplyr::select(data, s1, s2)))
+Sigma <- 2 * exp(-dist/0.02)
+z <- rnorm(n)
+microbenchmark(
+  mvtnorm::dmvnorm(z, mean=rep(0, n), sigma=Sigma, log = TRUE),
+  spmirt::dmvnorm(z, mean=rep(0, n), sigma=Sigma),
+  times = 10
+)
+
+# mvtnorm::dmvnorm(z, mean=rep(0, n), sigma=Sigma, log = TRUE)
+# spmirt::dmvnorm(z, mean=rep(0, n), sigma=Sigma)
