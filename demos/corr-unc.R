a825a91ed7d35b0761040be789706319a1e50642
Erick Chacon Montalvan <eral.th07@gmail.com>
2018-04-04 22:42:32 +0100

    conversion from cholesky of correlation to unconstrained parameters

--- demos/corr-unc.R
+++ demos/corr-unc.R
@@ -10,17 +10,17 @@ k <- 3
 N <- k * (k+1)/2
 x <- 1:N
 (bla <- vec2trimatl(x, 3, TRUE))
-class(bla)
+(x2 <- trimatl2vec(bla, TRUE))
 
 N <- k * (k-1)/2
 x <- 1:N
 (bla <- vec2trimatl(x, 3, FALSE))
-class(bla)
+(x2 <- trimatl2vec(bla, FALSE))
 
 N <- k * (k-1)/2
 x <- rnorm(N)
-(bla <- vec2corr(x, 3))
-class(bla)
+(bla <- vec2chol_corr(x, 3))
+(bla2 <- chol_corr2vec(bla))
 
 tcrossprod(bla)
 # t(chol(mycorr))
--- src/correlation.cpp
+++ src/correlation.cpp
@@ -5,7 +5,7 @@
 
 //' @export
 // [[Rcpp::export]]
-arma::mat vec2trimatl (arma::vec x, int K, bool diag = true) {
+arma::mat vec2trimatl(arma::vec x, int K, bool diag = true) {
   // K = (-1 + sqrt(1 + 8 * x.length))/2
   // K = (1 + sqrt(1 + 8 * x.length))/2
   arma::mat L(K, K, arma::fill::zeros);
@@ -16,7 +16,7 @@ arma::mat vec2trimatl (arma::vec x, int K, bool diag = true) {
     }
   } else {
     for (int i = 0; i < K-1; ++i) {
-      L.submat(i+1,i, K-1,i) = vecsub(x, round(i*K - (i+1)*(i)/2), K-i-1);
+      L.submat(i+1,i, K-1,i) = vecsub(x, round(i*K - (i+1)*i/2), K-i-1);
     }
   }
   return L;
@@ -24,12 +24,42 @@ arma::mat vec2trimatl (arma::vec x, int K, bool diag = true) {
 
 //' @export
 // [[Rcpp::export]]
-arma::mat vec2corr(arma::vec x, int K) {
+arma::vec trimatl2vec(arma::mat L, bool diag = true) {
+  int K = L.n_rows;
+  int N;
+
+  if (diag) {
+    N = round(K * (K + 1) / 2);
+  } else {
+    N = round((K - 1) * K / 2);
+  }
+
+  arma::vec x(N, arma::fill::zeros);
+  // fill vector by lower triangle column
+  if (diag) {
+    for (int i = 0; i < K; ++i) {
+      x.subvec(round(i*K - i*(i-1)/2), arma::size(K-i,1)) = L.submat(i,i, K-1,i);
+    }
+  } else {
+    for (int i = 0; i < K-1; ++i) {
+      x.subvec(round(i*K-(i+1)*i/2), arma::size(K-i-1, 1)) = L.submat(i+1,i, K-1,i);
+    }
+  }
+
+  return x;
+}
+
+
+//' @export
+// [[Rcpp::export]]
+arma::mat vec2chol_corr(arma::vec x, int K) {
   x = tanh(x);
   arma::mat L = vec2trimatl(x, K, false);
   arma::mat L_chol(K, K, arma::fill::zeros);
+
   L_chol.col(0) = L.col(0);
   L_chol(0,0) = 1.0;
+
   for (int i = 2; i < K; ++i) {
     for (int j = 1; j < i; ++j) {
       L_chol(i,j) = L(i,j) * sqrt(1 - accu(square(L_chol.submat(i,0, i,j-1))));
@@ -40,5 +70,27 @@ arma::mat vec2corr(arma::vec x, int K) {
   }
 
   return L_chol;
+}
+
+
+//' @export
+// [[Rcpp::export]]
+arma::vec chol_corr2vec(arma::mat L_chol) {
+  int K = L_chol.n_rows;
+  arma::mat L(K, K, arma::fill::zeros);
+
+  L.col(0) = L_chol.col(0);
+  L(0,0) = 0;
+
+  for (int i = 2; i < K; ++i) {
+    for (int j = 1; j < i; ++j) {
+      L(i,j) = L_chol(i,j) / sqrt(1 - accu(square(L_chol.submat(i,0, i,j-1))));
+    }
+  }
+
+  arma::vec x = trimatl2vec(L, false);
+  x = atanh(x);
+
+  return x;
 }
 
