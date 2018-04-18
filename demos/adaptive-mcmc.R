5a909f825d709e4d1e6c5d009eec6291a5144943
Erick Chacon Montalvan <eral.th07@gmail.com>
2018-04-18 01:33:47 +0100

    am algorithm with global adaptive scaling

--- demos/adaptive-mcmc.R
+++ demos/adaptive-mcmc.R
@@ -19,11 +19,10 @@ D_inv <- diag(d ^ (-1))
 Sigma <- D %*% Corr %*% D
 
 
-iter <- 900000
+iter <- 10^6
 system.time(
   samples <- adaptive_haario(mean, Sigma, iter)
 )
-
 
 samples_tib <- as_tibble.spmirt.list(samples, iter/2)
 summary(samples_tib)
@@ -35,8 +34,44 @@ as_tibble.spmirt.list(samples, 0, 500) %>%
 as_tibble.spmirt.list(samples, iter/2, 50) %>%
   gg_density_ridges(aes(fill = Parameters), scale = 6, alpha = 0.5, bandwidth = 0.25)
 
-nrow(unique(samples_tib)) / nrow(samples_tib)
-acf(samples_tib$V1, 500)
+system.time(
+  samples2 <- adaptive_haario_vanish(mean, Sigma, iter)
+)
+
+samples_tib2 <- as_tibble.spmirt.list(samples2, iter/2)
+summary(samples_tib2)
+samples_long2 <- gather(samples_tib2)
+
+as_tibble.spmirt.list(samples2, 0, 500) %>%
+  gg_trace(alpha = 0.6)
+
+as_tibble.spmirt.list(samples2, iter/2, 50) %>%
+  gg_density_ridges(aes(fill = Parameters), scale = 6, alpha = 0.5, bandwidth = 0.25)
+
+system.time(
+  samples3 <- am_vanish_scaling(mean, Sigma, iter)
+)
+
+samples_tib3 <- as_tibble.spmirt.list(samples3, iter/2)
+summary(samples_tib3)
+samples_long3 <- gather(samples_tib3)
+
+as_tibble.spmirt.list(samples3, 0, 500) %>%
+  gg_trace(alpha = 0.6)
+
+as_tibble.spmirt.list(samples3, iter/2, 50) %>%
+  gg_density_ridges(aes(fill = Parameters), scale = 6, alpha = 0.5, bandwidth = 0.25)
+
+
+# cov(samples_tib) - Sigma
+# cov(samples_tib2) - Sigma
+
+# nrow(unique(samples_tib)) / nrow(samples_tib)
+# acf(samples_tib$V3, 500)
+# acf(samples_tib2$V3, 500)
+# acf(samples_tib3$V3, 500)
+# acf(samples_tib$V2, 500)
+# acf(samples_tib2$V2, 500)
 
 # Covariance matrix recursive update
 # aux <- cbind(test[i,] - apply(test[-i, , drop = FALSE], 2, mean))
--- src/adaptive-mcmc.cpp
+++ src/adaptive-mcmc.cpp
@@ -29,21 +29,19 @@
 Rcpp::List adaptive_haario(arma::vec mean, arma::mat Sigma, int iter) {
   // Examples of Adaptive MCMC: Gareth O. Roberts and Jeffrey S. Rosenthal
 
+  // Constants
   const int n = mean.n_elem;
-  arma::mat eye_n = arma::eye<arma::mat>(n,n);
+  const arma::mat Sigma_chol = arma::chol(Sigma, "lower");
 
-  arma::mat Sigma_chol = arma::chol(Sigma, "lower");
-
-  // arma::vec params(n, arma::fill::randn);
-  arma::vec params = mean / 3;
-  arma::mat params_mat(n, iter);
-
+  // Initializing parameter variables
   arma::vec params_mean(n, arma::fill::zeros);
   arma::mat params_cov(n,n, arma::fill::zeros);
-
+  arma::vec params(n, arma::fill::randn);
+  arma::mat params_mat(n, iter);
 
   for (int i = 0; i < iter; ++i) {
 
+    // Define current Sigma_proposal
     arma::mat Sigma_proposal(n,n);
     if (i >= (2 * n) && R::runif(0,1) < 0.95) {
       Sigma_proposal = pow(2.38, 2) * params_cov / n;
@@ -51,6 +49,7 @@ Rcpp::List adaptive_haario(arma::vec mean, arma::mat Sigma, int iter) {
       Sigma_proposal = pow(0.1, 2) * arma::eye<arma::mat>(n,n) / n;
     }
 
+    // Update parameters
     arma::mat Sigma_proposal_chol;
     bool nonsingular = arma::chol(Sigma_proposal_chol, Sigma_proposal, "lower");
     if (nonsingular) {
@@ -63,6 +62,7 @@ Rcpp::List adaptive_haario(arma::vec mean, arma::mat Sigma, int iter) {
       }
     }
 
+    // Update covariance of parameters recursively
     if (i > 0) {
       arma::vec params_center = params - params_mean;
       params_cov = params_cov +
@@ -70,6 +70,7 @@ Rcpp::List adaptive_haario(arma::vec mean, arma::mat Sigma, int iter) {
     }
     params_mean = params_mean + (params - params_mean) / (i + 1);
 
+    // Store current parameter
     params_mat.col(i) = params;
   }
 
@@ -107,28 +108,27 @@ arma::mat variance(arma::mat X) {
 Rcpp::List adaptive_haario_vanish(arma::vec mean, arma::mat Sigma, int iter) {
   // A tutorial on adaptive MCMC: Christophe Andrieu and Johannes Thoms
 
+  // Constants
   const int n = mean.n_elem;
-  arma::mat eye_n = arma::eye<arma::mat>(n,n);
+  const arma::mat Sigma_chol = arma::chol(Sigma, "lower");
 
-  arma::mat Sigma_chol = arma::chol(Sigma, "lower");
-
-  // arma::vec params(n, arma::fill::randn);
-  arma::vec params = mean / 3;
+  // Initializing parameter variables
+  arma::vec params_mean(n, arma::fill::zeros);
+  arma::mat params_cov = 1 * arma::eye<arma::mat>(n,n);
+  arma::vec params(n, arma::fill::randn);
   arma::mat params_mat(n, iter);
 
-  arma::vec params_mean(n, arma::fill::zeros);
-  arma::mat params_cov(n,n, arma::fill::zeros);
-
+  // Adaptive stepsize
+  const double alpha = 0.8;
+  const double C = 0.9;
+  double gamma;
 
   for (int i = 0; i < iter; ++i) {
 
-    arma::mat Sigma_proposal(n,n);
-    if (i >= (2 * n) && R::runif(0,1) < 0.95) {
-      Sigma_proposal = pow(2.38, 2) * params_cov / n;
-    } else {
-      Sigma_proposal = pow(0.1, 2) * arma::eye<arma::mat>(n,n) / n;
-    }
+    // Define current Sigma_proposal
+    arma::mat Sigma_proposal = pow(2.38, 2) * params_cov / n;
 
+    // Update parameters
     arma::mat Sigma_proposal_chol;
     bool nonsingular = arma::chol(Sigma_proposal_chol, Sigma_proposal, "lower");
     if (nonsingular) {
@@ -141,13 +141,14 @@ Rcpp::List adaptive_haario_vanish(arma::vec mean, arma::mat Sigma, int iter) {
       }
     }
 
-    if (i > 0) {
-      arma::vec params_center = params - params_mean;
-      params_cov = params_cov +
-        (params_center * params_center.t() - (i+1)/i * params_cov) / (i + 1);
-    }
-    params_mean = params_mean + (params - params_mean) / (i + 1);
+    // Update covariance of parameters with stochastic approximation
+    gamma = C / pow(i+1, alpha);
+    arma::vec params_center = params - params_mean;
+    params_mean = params_mean + gamma * params_center;
+    params_cov = params_cov + gamma * (params_center * params_center.t() -
+        params_cov);
 
+    // Store current parameter
     params_mat.col(i) = params;
   }
 
@@ -155,4 +156,82 @@ Rcpp::List adaptive_haario_vanish(arma::vec mean, arma::mat Sigma, int iter) {
       Rcpp::Named("params") = params_mat.t()
       );
 }
+
+
+//' @title AM Algorithm with Global Adaptive Scaling by Christophe
+//'
+//' @description
+//' \code{ifa_gibbs} description.
+//'
+//' @details
+//' details.
+//'
+//' @param par.
+//'
+//' @return return.
+//'
+//' @author Erick A. Chacon-Montalvan
+//'
+//' @examples
+//'
+//'
+//' @export
+// [[Rcpp::export]]
+Rcpp::List am_vanish_scaling(arma::vec mean, arma::mat Sigma, int iter) {
+  // A tutorial on adaptive MCMC: Christophe Andrieu and Johannes Thoms
+
+  // Constants
+  const int n = mean.n_elem;
+  const arma::mat Sigma_chol = arma::chol(Sigma, "lower");
+
+  // Initializing parameter variables
+  arma::vec params_mean(n, arma::fill::zeros);
+  arma::mat params_cov = 1 * arma::eye<arma::mat>(n,n);
+  arma::vec params(n, arma::fill::randn);
+  arma::mat params_mat(n, iter);
+  double logscale = 0;
+
+  // Adaptive stepsize
+  const double alpha = 0.8;
+  const double C = 0.9;
+  double gamma;
+  double accept;
+
+  for (int i = 0; i < iter; ++i) {
+
+    // Define current Sigma_proposal
+    arma::mat Sigma_proposal = exp(logscale) * params_cov;
+
+    // Update parameters
+    arma::mat Sigma_proposal_chol;
+    bool nonsingular = arma::chol(Sigma_proposal_chol, Sigma_proposal, "lower");
+    if (nonsingular) {
+      arma::vec params_aux = params + Sigma_proposal_chol * arma::randn<arma::vec>(n);
+
+      accept = dmvnorm_chol(params_aux, mean, Sigma_chol, true) -
+        dmvnorm_chol(params, mean, Sigma_chol, true);
+      if (accept > log(R::runif(0,1))) {
+        params = params_aux;
+      }
+    }
+
+    // Update scaling parameter
+    gamma = C / pow(i+1, alpha);
+    logscale += gamma * (std::min(exp(accept), 1.0) - 0.234);
+
+    // Update covariance of parameters with stochastic approximation
+    arma::vec params_center = params - params_mean;
+    params_mean = params_mean + gamma * params_center;
+    params_cov = params_cov + gamma * (params_center * params_center.t() -
+        params_cov);
+
+    // Store current parameter
+    params_mat.col(i) = params;
+  }
+
+  return Rcpp::List::create(
+      Rcpp::Named("params") = params_mat.t()
+      );
+}
+
 
