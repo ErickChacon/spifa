5fb0dab16d01e3a84d3f45be1a143319695ac91b
Erick Chacon Montalvan <eral.th07@gmail.com>
2018-04-30 17:06:57 +0100

    default restriction and priors in A for exploratory and confirmatory

--- R/spmirt.R
+++ R/spmirt.R
@@ -1,10 +1,14 @@
 
-spmirt <- function (response,
-                    nobs, nitems, nfactors, niter = 100,
-                    constrains = list(A = NULL, W = NULL),
-                    c_opt = list(initial = NULL, prior_mean = NULL, prior_sd = NULL),
-                    A_opt = list(initial = NULL, prior_mean = NULL, prior_sd = NULL),
-                    theta_init) {
+spmirt <- function (response, predictors = NULL, coordinates = NULL,
+    nobs, nitems, nfactors, niter = 100,
+    constrains = list(A = NULL, W = NULL),
+    c_opt = list(initial = NULL, prior_mean = NULL, prior_sd = NULL),
+    A_opt = list(initial = NULL, prior_mean = NULL, prior_sd = NULL),
+    R_opt = list(initial = NULL, prior_mean = NULL, prior_sd = NULL),
+    B_opt = list(initial = NULL, prior_mean = NULL, prior_sd = NULL),
+    sigmas_gp_opt = list(initial = NULL, prior_mean = NULL, prior_sd = NULL),
+    phi_gp_opt = list(initial = NULL, prior_mean = NULL, prior_sd = NULL)
+    ) {
 
   # Optional arguments for parameter c, checking adequate sizes
 
@@ -48,6 +52,7 @@ spmirt <- function (response,
 
   if (is.null(A_opt$prior_mean)) {
     A_prior_mean <- matrix(0, nitems, nfactors)
+    diag(A_prior_mean) <- 1
   } else {
     if (length(A_opt$prior_mean) == 1) {
       A_prior_mean <- matrix(A_opt$prior_mean, nitems, nfactors)
@@ -61,6 +66,9 @@ spmirt <- function (response,
 
   if (is.null(A_opt$prior_sd)) {
     A_prior_sd <- matrix(1, nitems, nfactors)
+    if (is.null(A_opt$prior_sd)) {
+      diag(A_prior_sd) <- 0.45
+    }
   } else {
     if (length(A_opt$prior_sd) == 1) {
       A_prior_sd <- matrix(A_opt$prior_sd, nitems, nfactors)
@@ -73,11 +81,7 @@ spmirt <- function (response,
   }
 
   if (is.null(A_opt$initial)) {
-    if (is.null(A_opt$prior_mean)) {
-      A_initial <- matrix(1, nitems, nfactors)
-    } else {
       A_initial <- A_prior_mean
-    }
   } else {
     if (length(A_opt$initial) == 1) {
       A_initial <- matrix(A_opt$initial, nitems, nfactors)
@@ -104,14 +108,15 @@ spmirt <- function (response,
 
   # Restrictions arguments for Gaussian Processes W(s)
 
-  samples <- spmirt_cpp(
-    response = response,
-    nobs = nobs, nitems = nitems, nfactors = nfactors, niter = niter,
-    constrain_L = constrain_L,
-    c_initial = c_initial, c_prior_mean = c_prior_mean, c_prior_sd = c_prior_sd,
-    A_initial = A_initial, A_prior_mean = A_prior_mean, A_prior_sd = A_prior_sd,
-    theta_init = theta_init
-    )
-  return(samples)
-  # return(constrain_L)
+  # samples <- spmirt_cpp(
+  #   response = response,
+  #   nobs = nobs, nitems = nitems, nfactors = nfactors, niter = niter,
+  #   constrain_L = constrain_L,
+  #   c_initial = c_initial, c_prior_mean = c_prior_mean, c_prior_sd = c_prior_sd,
+  #   A_initial = A_initial, A_prior_mean = A_prior_mean, A_prior_sd = A_prior_sd,
+  #   theta_init = theta_init
+  #   )
+  # return(samples)
+  return(list(constrain_L = constrain_L, A_initial = A_initial,
+              A_prior_mean = A_prior_mean, A_prior_sd =  A_prior_sd))
 }
