13afe7cf9fe623d67f24efca0de8bd0ec46cad4c
Erick Chacon <eral.th07@gmail.com>
2018-11-28 18:20:39 +0000

    random walk of order 2 on a lattice with 2 dimensions

--- demos/gam-rw.R
+++ demos/gam-rw.R
@@ -54,3 +54,23 @@ opar <- par(mar = c(0.5, 0.5, 0.5, 0.5))
 plot(r1, col = colorRampPalette(brewer.pal(11, "Spectral"))(30), axes = FALSE, box = FALSE)
 plot(rasterToPolygons(r1), add = TRUE, border = "black", lwd = 1)
 par(opar)
+
+# RANDOM WALK 2 ON A LATTICE ---------------------------------------------------
+
+tao <- 1
+n <- 30
+r1 <- raster(nrows = n, ncols = n, xmn = 0, xmx = n, ymn = 0, ymx = n)
+ncell1 <- ncell(r1)
+index <- 1:ncell1
+adj <- adjacent(r1, cells = index, 4, sorted = TRUE, pairs = TRUE)
+adj_tab <- as.numeric(table(adj[, 1]))
+Adj <- matrix(0, ncell1, ncell1)
+Adj[adj] <- 1
+diag(Adj) <- - adj_tab
+Q <- crossprod(Adj) / tao ^ 2
+r1 <- setValues(r1, rimvnorm_Q_eig(Q))
+opar <- par(mar = c(0.5, 0.5, 0.5, 0.5))
+plot(r1, col = colorRampPalette(brewer.pal(11, "Spectral"))(30), axes = FALSE, box = FALSE)
+plot(rasterToPolygons(r1), add = TRUE, border = "black", lwd = 1)
+par(opar)
+
