b8c3ae8622f396f8d6bf59b77f9c99e1d800e7a6
Erick Chacon Montalvan <eral.th07@gmail.com>
2020-03-23 18:08:04 +0000

    move laptop

---
+++ spifa.R
@@ -0,0 +1,11 @@
+
+Rcpp::compileAttributes("..")                                         # cpp to R
+devtools::load_all("..")                                              # compile src code before document
+roxygen2::roxygenize("..", roclets = c("collate", "namespace", "rd")) # document
+
+devtools::check_built("..")
+
+
+
+devtools::load_all("..")
+
