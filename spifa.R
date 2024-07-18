96333e8800071d464be5dcb3525af8550874a10f
Erick <eral.th07@gmail.com>
2024-07-18 09:42:45 +0300

    update package for paper submission

--- spifa.R
+++ spifa.R
@@ -1,11 +1,11 @@
 
-Rcpp::compileAttributes("..")                                         # cpp to R
-devtools::load_all("..")                                              # compile src code before document
-roxygen2::roxygenize("..", roclets = c("collate", "namespace", "rd")) # document
+Rcpp::compileAttributes(".", verbose = TRUE)                                         # cpp to R
+devtools::load_all(".")                                              # compile src code before document
+roxygen2::roxygenize(".", roclets = c("collate", "namespace", "rd")) # document
 
-devtools::check_built("..")
+devtools::check_built(".")
 
 
 
-devtools::load_all("..")
+devtools::load_all(".")
 
